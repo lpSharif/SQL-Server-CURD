37b12696514fa78f448b7b24ba80152b4d81d116
Sharif Ahamed <sharifahamed28.sa@gmail.com>
2020-09-27 21:49:24 +0600

    form file added

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Database_Management_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.CustomerTable = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.insert = new System.Windows.Forms.Button();
+            this.idbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fstnamebox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.agebox = new System.Windows.Forms.TextBox();
+            this.citybox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstnamebox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.button1.Location = new System.Drawing.Point(435, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CustomerTable
+            // 
+            this.CustomerTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.CustomerTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomerTable.Location = new System.Drawing.Point(63, 240);
+            this.CustomerTable.Name = "CustomerTable";
+            this.CustomerTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.CustomerTable.Size = new System.Drawing.Size(690, 244);
+            this.CustomerTable.TabIndex = 1;
+            this.CustomerTable.Text = "dataGridView1";
+            this.CustomerTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CustomerTable_CellClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.button2.Location = new System.Drawing.Point(647, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 41);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.button3.Location = new System.Drawing.Point(232, 193);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 41);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // insert
+            // 
+            this.insert.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.insert.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.insert.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.insert.Location = new System.Drawing.Point(63, 193);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(106, 41);
+            this.insert.TabIndex = 0;
+            this.insert.Text = "Insert";
+            this.insert.UseVisualStyleBackColor = false;
+            this.insert.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // idbox
+            // 
+            this.idbox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.idbox.Enabled = false;
+            this.idbox.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.idbox.Location = new System.Drawing.Point(153, 33);
+            this.idbox.Name = "idbox";
+            this.idbox.ReadOnly = true;
+            this.idbox.Size = new System.Drawing.Size(60, 31);
+            this.idbox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(31, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Customer ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // fstnamebox
+            // 
+            this.fstnamebox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.fstnamebox.Location = new System.Drawing.Point(411, 31);
+            this.fstnamebox.Name = "fstnamebox";
+            this.fstnamebox.Size = new System.Drawing.Size(130, 27);
+            this.fstnamebox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(257, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Customer Name";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // agebox
+            // 
+            this.agebox.Location = new System.Drawing.Point(153, 96);
+            this.agebox.Name = "agebox";
+            this.agebox.Size = new System.Drawing.Size(60, 23);
+            this.agebox.TabIndex = 2;
+            // 
+            // citybox
+            // 
+            this.citybox.Location = new System.Drawing.Point(411, 96);
+            this.citybox.Name = "citybox";
+            this.citybox.Size = new System.Drawing.Size(148, 23);
+            this.citybox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(350, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "City";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(102, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Age";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lstnamebox
+            // 
+            this.lstnamebox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstnamebox.Location = new System.Drawing.Point(543, 31);
+            this.lstnamebox.Name = "lstnamebox";
+            this.lstnamebox.Size = new System.Drawing.Size(130, 27);
+            this.lstnamebox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(818, 480);
+            this.Controls.Add(this.lstnamebox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.citybox);
+            this.Controls.Add(this.agebox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fstnamebox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.idbox);
+            this.Controls.Add(this.insert);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.CustomerTable);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Customer Management System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView CustomerTable;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button insert;
+        private System.Windows.Forms.TextBox idbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fstnamebox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox agebox;
+        private System.Windows.Forms.TextBox citybox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox lstnamebox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Database_Management_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=LPSHARIF;Initial Catalog=Customer;Integrated Security=True");
+
+        public int customerID;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getCustomerRecord();
+        }
+
+        private void getCustomerRecord()
+        {
+            SqlCommand cmd = new SqlCommand("select * from Customer", con);
+            DataTable dt = new DataTable();
+            con.Open();
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+
+            CustomerTable.DataSource = dt;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (isVaild())
+            {
+                SqlCommand cmd = new SqlCommand("insert into Customer values (@firstName, @lastName,@age,@city)", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@firstName", fstnamebox.Text);
+                cmd.Parameters.AddWithValue("@lastName", lstnamebox.Text);
+                cmd.Parameters.AddWithValue("@age", agebox.Text);
+                cmd.Parameters.AddWithValue("@city", citybox.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("New Customer Added Successfully!", "Added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                getCustomerRecord();
+                refreshBoxs();
+            }
+        }
+
+        private bool isVaild()
+        {
+            if (fstnamebox.Text == string.Empty)
+            {
+                MessageBox.Show("Customer's First Name is required", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if (lstnamebox.Text == string.Empty)
+            {
+                MessageBox.Show("Customer's Last Name is required", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if (agebox.Text == string.Empty)
+            {
+                MessageBox.Show("Age is required", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if (citybox.Text == string.Empty)
+            {
+                MessageBox.Show("City name is required", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            refreshBoxs();
+        }
+
+        private void refreshBoxs()
+        {
+            customerID = 0;
+            fstnamebox.Clear();
+            lstnamebox.Clear();
+            agebox.Clear();
+            citybox.Clear();
+            idbox.Clear();
+        }
+
+        private void CustomerTable_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            customerID = Convert.ToInt32(CustomerTable.SelectedRows[0].Cells[0].Value);
+            idbox.Text = CustomerTable.SelectedRows[0].Cells[0].Value.ToString();
+            fstnamebox.Text = CustomerTable.SelectedRows[0].Cells[1].Value.ToString();
+            lstnamebox.Text = CustomerTable.SelectedRows[0].Cells[2].Value.ToString();
+            agebox.Text = CustomerTable.SelectedRows[0].Cells[3].Value.ToString();
+            citybox.Text = CustomerTable.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        private bool executeable()
+        {
+            if (customerID == Convert.ToInt32(idbox.Text))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (customerID > 0)
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE Customer SET firstName = @firstName, lastName = @lastName,age = @age,city = @city WHERE customerId = @customerId", con);
+                cmd.CommandType = CommandType.Text;
+                
+                cmd.Parameters.AddWithValue("@firstName", fstnamebox.Text);
+                cmd.Parameters.AddWithValue("@lastName", lstnamebox.Text);
+                cmd.Parameters.AddWithValue("@age", agebox.Text);
+                cmd.Parameters.AddWithValue("@city", citybox.Text);
+                cmd.Parameters.AddWithValue("@customerId", this.customerID);
+
+                bool ok = executeable();
+
+                if (ok)
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    MessageBox.Show("Customer's Details Updated Successfully!", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    getCustomerRecord();
+                    refreshBoxs();
+                }
+                else
+                {
+                    MessageBox.Show("Can't Change Customer ID :(", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Please Select A Customer!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (customerID > 0)
+            {
+                SqlCommand cmd = new SqlCommand("DELETE FROM Customer WHERE customerId = @customerId", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@customerId", this.customerID);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Customer's Record Deleted Successfully!", "Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                getCustomerRecord();
+                refreshBoxs();
+            }
+            else
+            {
+                MessageBox.Show("Please Select A Customer!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
